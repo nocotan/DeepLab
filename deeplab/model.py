5a1f2365de2c549a8e0ccc8f2f62fb0c236cc62e
kimura <noconoco.lib@gmail.com>
2018-03-12 18:50:26 +0900

    modify

--- deeplab/model.py
+++ deeplab/model.py
@@ -73,30 +73,6 @@ class Classifier_Module(nn.Module):
         return out
 
 
-class Residual_Covolution(nn.Module):
-    def __init__(self, icol, ocol, num_classes):
-        super(Residual_Covolution, self).__init__()
-        self.conv1 = nn.Conv2d(icol, ocol, kernel_size=3, stride=1, padding=12,
-                               dilation=12, bias=True)
-        self.conv2 = nn.Conv2d(ocol, num_classes, kernel_size=3, stride=1,
-                               padding=12, dilation=12, bias=True)
-        self.conv3 = nn.Conv2d(num_classes, ocol, kernel_size=1, stride=1,
-                               padding=0, dilation=1, bias=True)
-        self.conv4 = nn.Conv2d(ocol, icol, kernel_size=1, stride=1, padding=0,
-                               dilation=1, bias=True)
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        dow1 = self.conv1(x)
-        dow1 = self.relu(dow1)
-        seg = self.conv2(dow1)
-        inc1 = self.conv3(seg)
-        add1 = dow1 + self.relu(inc1)
-        inc2 = self.conv4(add1)
-        out = x + self.relu(inc2)
-        return out, seg
-
-
 class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes):
         self.inplanes = 64
